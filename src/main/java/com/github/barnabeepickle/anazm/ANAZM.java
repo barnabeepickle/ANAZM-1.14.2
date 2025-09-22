2e0809ac879727cd34c22e99b9da4d41950ed87a
barnabeepickle <linusrk30@gmail.com>
2025-09-22 13:42:25 -0400

    basic additions from logiczoom

--- src/main/java/com/github/barnabeepickle/anazm/ANAZM.java
+++ src/main/java/com/github/barnabeepickle/anazm/ANAZM.java
@@ -2,6 +2,7 @@ package com.github.barnabeepickle.anazm;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -9,10 +10,83 @@ import org.lwjgl.glfw.GLFW;
 public class ANAZM implements ClientModInitializer {
 	public static KeyBinding zoomKeyBinding;
 	// For hooking the zoom event
-	public static boolean isZooming = false;
+	public static boolean isZooming;
+	private static boolean originalSmoothCameraEnabled;
+	private static final MinecraftClient mc = MinecraftClient.getInstance();
+
+	public static final float zoomLevel = (float) 0.23;
 
 	@Override
 	public void onInitializeClient() {
-		zoomKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.anazm.zoom", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "category.anazm.main"));
+		zoomKeyBinding = new KeyBinding("key.anazm.zoom", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "category.anazm.main");
+		
+		isZooming = false;
+
+		KeyBindingHelper.registerKeyBinding(zoomKeyBinding);
 	}
+
+	public static boolean isZooming() {
+        return zoomKeyBinding.isPressed();
+    }
+
+    public static void toggleSmoothCamera() {
+        if (zoomStarting()) {
+            zoomStarted();
+            enableSmoothCamera();
+        }
+
+        if (zoomStopping()) {
+            zoomStopped();
+            resetSmoothCamera();
+        }
+    }
+
+	 private static boolean isSmoothCamera() {
+        return mc.options.smoothCameraEnabled;
+    }
+
+    private static void enableSmoothCamera() {
+        try {
+			mc.options.smoothCameraEnabled = true;
+		} catch (Exception e) {
+			System.out.println("Failed to enable smoothCamera");
+			e.printStackTrace();
+		}
+    }
+
+    private static void disableSmoothCamera() {
+        try {
+			mc.options.smoothCameraEnabled = false;
+		} catch (Exception e) {
+			System.out.println("Failed to diable smoothCamera");
+			e.printStackTrace();
+		}
+    }
+
+	private static boolean zoomStarting() {
+        return isZooming() && !isZooming;
+    }
+
+    private static boolean zoomStopping() {
+        return !isZooming() && isZooming;
+    }
+
+
+    private static void zoomStarted() {
+        originalSmoothCameraEnabled = isSmoothCamera();
+        isZooming = true;
+    }
+
+	private static void zoomStopped() {
+        isZooming = false;
+    }
+
+
+	private static void resetSmoothCamera() {
+        if (originalSmoothCameraEnabled) {
+            enableSmoothCamera();
+        } else {
+            disableSmoothCamera();
+        }
+    }
 }
--- src/main/java/com/github/barnabeepickle/anazm/mixin/GameRendererMixin.java
+++ src/main/java/com/github/barnabeepickle/anazm/mixin/GameRendererMixin.java
@@ -1,18 +1,29 @@
 package com.github.barnabeepickle.anazm.mixin;
 
 import com.github.barnabeepickle.anazm.ANAZM;
-import net.minecraft.client.render.GameRenderer;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.client.render.GameRenderer;
+
+@Environment(EnvType.CLIENT)
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
     @ModifyVariable(method = "getFov", at = @At(value = "RETURN", shift = At.Shift.BEFORE), ordinal = 0)
-    private double getFov(double fov) {
-        double d;
-        ANAZM.isZooming = ANAZM.zoomKeyBinding.isPressed();
-        d = ANAZM.isZooming ? 9.0 : 1.0;
-        return fov / d;
+    @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
+    public void getZoomLevel(CallbackInfoReturnable<Float> callbackInfo) {
+        if(ANAZM.isZooming()) {
+            float fov = callbackInfo.getReturnValue();
+            callbackInfo.setReturnValue(fov * ANAZM.zoomLevel);
+        }
+
+        ANAZM.toggleSmoothCamera();
     }
 }
